e7ec1ecc1c1612e8d32cb3d708ecac2f85fb2864
Clorith <bg3erl42>
2018-10-16 23:05:04 +0200

    Add `GM_getResourceText` to the grants, lost in the transition to GitHub

--- src/wordpress-plugins-topic-highlighter.user.js
+++ src/wordpress-plugins-topic-highlighter.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         WordPress.org plugins and themes topic highlighter
 // @namespace    http://clorith.net/
-// @version      0.3.1
+// @version      0.3.2
 // @description  Add status highlights to topics for easy overviews.
 // @author       Clorith
 // @match        https://wordpress.org/support/plugin/*
 // @match        https://wordpress.org/support/theme/*
 // @require      https://code.jquery.com/jquery-1.11.0.min.js
 // @resource     configHtml https://raw.githubusercontent.com/Clorith/wporg-topic-highlighter/master/src/options.html
-// @grant        none
+// @grant        GM_getResourceText
 // ==/UserScript==
 
 jQuery(document).ready(function( $ ) {
