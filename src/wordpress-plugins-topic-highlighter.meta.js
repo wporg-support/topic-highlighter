5bf3ff41c93bbb4e0759adf979b858bbb70a7f81
Clorith <bg3erl42>
2018-10-16 22:57:13 +0200

    Update options page reference

--- src/wordpress-plugins-topic-highlighter.meta.js
+++ src/wordpress-plugins-topic-highlighter.meta.js
@@ -7,6 +7,6 @@
 // @match        https://wordpress.org/support/plugin/*
 // @match        https://wordpress.org/support/theme/*
 // @require      https://code.jquery.com/jquery-1.11.0.min.js
-// @resource     configHtml https://gist.githubusercontent.com/Clorith/25846220b2815c727e85ab36324d0af6/raw/1744684e556f933a1def851dc760f207cb554df9/options.html
+// @resource     configHtml https://raw.githubusercontent.com/Clorith/wporg-topic-highlighter/master/src/options.html
 // @grant        none
 // ==/UserScript==
