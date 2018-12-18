f6bf225c782ae9bb9e3a4f23ede6c7b099f15e0b
Marius L. Jensen <funman@gmail.com>
2018-12-18 13:02:24 +0100

    Also match for rosetta sites

--- src/wordpress-plugins-topic-highlighter.meta.js
+++ src/wordpress-plugins-topic-highlighter.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         WordPress.org plugins and themes topic highlighter
 // @namespace    http://clorith.net/
-// @version      0.3.3
+// @version      0.3.4
 // @description  Add status highlights to topics for easy overviews.
 // @author       Clorith
-// @match        https://wordpress.org/support/plugin/*
-// @match        https://wordpress.org/support/theme/*
+// @match        https://wordpress.org/support/*
+// @match        https://*.wordpress.org/support/*
 // @require      https://code.jquery.com/jquery-1.11.0.min.js
 // @resource     configHtml https://raw.githubusercontent.com/Clorith/wporg-topic-highlighter/master/src/options.html
 // @grant        GM_getResourceText
