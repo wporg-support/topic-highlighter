31209f24aeefb9eb21fe3365e08b5fc47b6a4b10
Marius Jensen <marius@clorith.net>
2021-03-09 09:09:28 +0100

    Version bump and set update paths for future rename of `master` branch

--- src/wordpress-plugins-topic-highlighter.meta.js
+++ src/wordpress-plugins-topic-highlighter.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         WordPress.org plugins and themes topic highlighter
 // @namespace    http://clorith.net/
-// @version      0.4.0
+// @version      0.5.0
 // @description  Add status highlights to topics for easy overviews.
 // @author       Clorith
 // @match        https://wordpress.org/support/*
 // @match        https://*.wordpress.org/support/*
 // @require      https://code.jquery.com/jquery-1.11.0.min.js
-// @resource     configHtml https://raw.githubusercontent.com/wporg-support/topic-highlighter/master/src/options.html
-// @updateURL    https://github.com/wporg-support/topic-highlighter/raw/master/src/wordpress-plugins-topic-highlighter.user.js
-// @downloadURL  https://github.com/wporg-support/topic-highlighter/raw/master/src/wordpress-plugins-topic-highlighter.user.js
+// @resource     configHtml https://raw.githubusercontent.com/wporg-support/topic-highlighter/trunk/src/options.html
+// @updateURL    https://github.com/wporg-support/topic-highlighter/raw/trunk/src/wordpress-plugins-topic-highlighter.user.js
+// @downloadURL  https://github.com/wporg-support/topic-highlighter/raw/trunk/src/wordpress-plugins-topic-highlighter.user.js
 // @grant        GM_getResourceText
 // ==/UserScript==
